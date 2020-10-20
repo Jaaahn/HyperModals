bdfd3ab572298e8de5e25d97a9638f5dfb5b6239
jan-b-photography <jan@bahlinger.family>
2020-10-20 17:41:39 +0200

    Notifications

---
+++ Readme.md
@@ -0,0 +1,39 @@
+# UI-Modal-Library
+
+(Yeah, we're searching for a better name)
+
+# Installation / CDN Setup
+
+Coming soon
+
+# The config object:
+
+The config object is the attribute that you pass to the class when creating a new instance.  
+The config has several options wich are mainly opional.  
+Details on what options are available can be found in the detailed documentation of each modal type below.
+
+# Creating a Notification
+
+## Simple example:
+
+```
+let test = new ModalsNotification({
+    text: "Hello, how are you?",
+});
+```
+
+## Config object
+
+-   `el` => The DOM element where the Notification should be attached
+-   `text` => The Text that should be displayed in the notification (NOT optional)
+-   `icon`: => JavaScript Object which can either contain a `class` or a `url` property. The url will be rendered in a `<img>` HTML Element
+-   `closeBtn` => Boolean, controls if a close button is shown or not
+-   `theme` => `light` or `dark`. Mainly affects the background color (See below for more information about custom CSS)
+-   `delay` => The delay the notification will wait before appearing on the users screen
+-   `duration` => Time the notification will stay visible to the user
+
+# TODOS:
+
+-   [ ] Make it NPM and CDN compatible
+-   [ ] Add Modals
+-   [ ] Notification: Fix Fontawesome Bug when enter animation is done and it snaps into position
---
+++ src/Modals.js
@@ -0,0 +1,71 @@
+// -------------------
+// Notifications
+// -------------------
+class ModalsNotification {
+    constructor(config) {
+        this.element = config.el || "body";
+        this.text = config.text;
+        this.icon = config.icon || {};
+        this.closeBtn = config.closeBtn || true;
+        this.theme = config.theme || "light";
+        this.delay = config.delay || 0;
+        this.duration = config.duration || 1000 * 5;
+
+        if (!this.text) {
+            console.error("Error: config.text is undefined.");
+            console.warn("Try something like this: \n\nlet modal = new ModalsNotification({ text: 'This is the displayed text' }); \n\nFor more info visit: GITHUB LINK COMING SOON");
+            return;
+        }
+
+        this.notificationElement;
+
+        setTimeout(() => this.present(), this.delay);
+        setTimeout(() => this.close(), this.delay + this.duration);
+    }
+
+    present() {
+        // Create container / background
+        let notification = document.createElement("div");
+        notification.classList.add("modal-notification");
+        notification.classList.add(this.theme);
+
+        // Create main text
+        let text = document.createElement("p");
+        text.innerText = this.text;
+
+        // Create icon if specified
+        let icon;
+        if (this.icon.hasOwnProperty("url")) {
+            icon = document.createElement("img");
+            icon.src = this.icon.url;
+        } else if (this.icon.hasOwnProperty("class")) {
+            icon = document.createElement("i");
+            this.icon.class.split(" ").forEach((e) => icon.classList.add(e));
+        }
+
+        // Create close btn if specified
+        let closeBtn;
+        if (this.closeBtn) {
+            closeBtn = document.createElement("button");
+            closeBtn.innerText = "X";
+            closeBtn.addEventListener("click", this.close);
+        }
+
+        // Add Elements to Container
+        if (icon) notification.appendChild(icon);
+        notification.appendChild(text);
+        if (closeBtn) notification.appendChild(closeBtn);
+
+        this.notificationElement = notification;
+
+        // Append to DOM
+        document.querySelector(this.element).appendChild(notification);
+    }
+
+    close() {
+        this.notificationElement.classList.add("exit");
+        setTimeout(() => {
+            document.querySelector(this.element).removeChild(this.notificationElement);
+        }, 1000 * 0.6);
+    }
+}
